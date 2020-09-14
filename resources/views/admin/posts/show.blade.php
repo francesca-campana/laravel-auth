8b5a44a8d3f9b9a1f6b8fe2009ea6b5e7a3c4fc4
francesca-campana <franx3rox@gmail.com>
2020-09-14 18:38:09 +0200

    Version 0.4

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,14 +4,23 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h2>Benvenuto nella lista dei post</h2>
+        <div class="media">
+          <div class="media-left media-middle">
+            <img class="media-object" src="{{$post->image}}" alt="{{ $post->title }}">
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">{{ $post->title }}</h4>
+            <p>{{ $post->content }}</p>
+            <a class="btn btn-primary" href="{{ route('admin.posts.index')}}"> Torna alla lista post</a>
+            <a class="btn btn-warning" href=""> Modifica Post</a>
+
+          </div>
+
+        </div>
+
+        </div>
       </div>
-      <ul>
-        @foreach ($posts as $post)
-          <li>{{ $post->user->name }} - {{ $post->title }}</li>
-          <a class="btn btn-success btn-xs" href="{{ route('admin.posts.show', $post) }}">Show</a>
-        @endforeach
-      </ul>
+
 
     </div>
 
